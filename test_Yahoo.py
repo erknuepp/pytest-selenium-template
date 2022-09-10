9cfde55ed9ad739e3addc603afc69c5e42d2c968
Edward Knueppel <edward.knueppel@zywave.com>
2022-09-10 07:28:48 -0500

    Removes setting deprecated executable_path property

--- test_Yahoo.py
+++ test_Yahoo.py
@@ -3,7 +3,8 @@ from selenium import webdriver as wd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.chrome.service import Service
 
 
 class TestYahoo:
@@ -18,23 +19,22 @@ class TestYahoo:
     @pytest.fixture
     def driver(self):
         path = r"C:\WebDrivers\chromedriver.exe"
-        self.driver = wd.Chrome(path)
+        s = Service(path)
+        self.driver = wd.Chrome(service=s)
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 10)
+        self.driver.get('http://www.yahoo.com')
         yield self.driver
         self.driver.quit()
 
     def test_yahoo_title(self, driver):
-        driver.get('http://www.yahoo.com')
         assert 'Yahoo' in driver.title
 
     def test_yahoo_url(self, driver):
-        driver.get('http://www.yahoo.com')
         assert 'https://www.yahoo.com' in driver.current_url
 
     def test_yahoo_search(self, driver):
-        driver.get('http://www.yahoo.com')
         driver.find_element(By.ID, "ybar-sbq").send_keys("Eliza Fletcher" + Keys.ENTER)
-        self.wait.until(EC.presence_of_element_located((By.ID, "results")))
+        self.wait.until(ec.presence_of_element_located((By.ID, "results")))
         element = driver.find_element(By.ID, "results")
         assert "Top Stories" in element.text
