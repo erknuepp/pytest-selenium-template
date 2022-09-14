5bcaecf3713ad719794ab5fdfcede3d9f7e31d78
Edward Knueppel <edward.knueppel@zywave.com>
2022-09-14 12:25:59 -0500

    Removes setting deprecated executable_path property

--- test_Yahoo.py
+++ test_Yahoo.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.chrome.service import Service
-
+import page
 
 class TestYahoo:
     wait = None
@@ -17,24 +17,39 @@ class TestYahoo:
     after the test completes, everything under the yield will execute
     '''
     @pytest.fixture
-    def driver(self):
+    def yahoo_driver(self):
         path = r"C:\WebDrivers\chromedriver.exe"
         s = Service(path)
         self.driver = wd.Chrome(service=s)
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 10)
-        self.driver.get('http://www.yahoo.com')
+        self.driver.get('https://www.yahoo.com')
         yield self.driver
         self.driver.quit()
 
-    def test_yahoo_title(self, driver):
-        assert 'Yahoo' in driver.title
+    @pytest.fixture
+    def python_driver(self):
+        path = r"C:\WebDrivers\chromedriver.exe"
+        s = Service(path)
+        self.driver = wd.Chrome(service=s)
+        self.driver.maximize_window()
+        self.wait = WebDriverWait(self.driver, 10)
+        self.driver.get('https://www.python.org')
+        yield self.driver
+        self.driver.quit()
 
-    def test_yahoo_url(self, driver):
-        assert 'https://www.yahoo.com' in driver.current_url
+    def test_yahoo_title(self, yahoo_driver):
+        assert 'Yahoo' in yahoo_driver.title
 
-    def test_yahoo_search(self, driver):
-        driver.find_element(By.ID, "ybar-sbq").send_keys("Eliza Fletcher" + Keys.ENTER)
+    def test_yahoo_url(self, yahoo_driver):
+        assert 'https://www.yahoo.com' in yahoo_driver.current_url
+
+    def test_yahoo_search(self, yahoo_driver):
+        yahoo_driver.find_element(By.ID, "ybar-sbq").send_keys("Eliza Fletcher" + Keys.ENTER)
         self.wait.until(ec.presence_of_element_located((By.ID, "results")))
-        element = driver.find_element(By.ID, "results")
+        element = yahoo_driver.find_element(By.ID, "results")
         assert "Top Stories" in element.text
+
+    def test_python(self, python_driver):
+        main_page = page.MainPage(python_driver)
+        assert main_page.is_title_matches() is True
